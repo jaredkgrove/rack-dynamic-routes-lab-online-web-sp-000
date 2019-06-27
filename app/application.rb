0d3e70e2424e1bf492ea204e73ae4edc837204d1
Jared Grove <jaredkgrove@gmail.com>
2019-06-27 19:53:22 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,8 +6,9 @@ class Application
 
     if req.path.match("/items/")
       item_name = req.path.split("/items/").last
+      item = @@items.detect{|item| item.name == item_name}
       if @@items.include?(item_name)
-        item = @@items.detect{|item| item.name == item_name}
+
         resp.write item.price
       else
         resp.write "Item not found"
