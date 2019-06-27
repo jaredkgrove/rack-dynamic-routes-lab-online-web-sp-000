fb0f5aae9b23d2118fc93bd1999898c47a5d42fb
Jared Grove <jaredkgrove@gmail.com>
2019-06-27 19:47:22 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,8 +5,12 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match("/items/")
-      item = req.path.split("/items/").last
-      if @@items.inculde?()
+      item_name = req.path.split("/items/").last
+      if @@items.inculde?(item_name)
+        resp.write @@items.detect(|item| item.name = item_name).name
+      else
+        req.status = 400
+      end
     else
       req.status = 404
     end
