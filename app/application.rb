967194cf849c3ec6266627a8266a4b0baff01ffc
Jared Grove <jaredkgrove@gmail.com>
2019-06-27 19:48:06 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
     if req.path.match("/items/")
       item_name = req.path.split("/items/").last
       if @@items.inculde?(item_name)
-        resp.write @@items.detect(|item| item.name == item_name).name
+        resp.write @@items.detect{|item| item.name == item_name}.name
       else
         req.status = 400
       end
